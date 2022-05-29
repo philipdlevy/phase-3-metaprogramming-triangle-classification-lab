bd506af713baf1e4a68b37f20fd4fb8294c79fd7
Philip Levy <philipdlevy@gmail.com>
2022-05-29 12:36:45 -0600

    commit tree 61388eeb833c6306f7af225f597129f53732ae43

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
+require 'pry'
+
 class Triangle
+  attr_accessor :side_one, :side_two, :side_three
   # write code here
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  def kind
+    # binding.pry
+    greater_than_0_error?
+    triangle_inequality_error?
+    if side_one == side_two && side_two == side_three
+      :equilateral
+    elsif side_one == side_two || side_two == side_three || side_one == side_three
+      :isosceles
+    else side_one != side_two && side_two != side_three
+      :scalene
+    end
+  end
+
+  # raise TriangleError
+  def greater_than_0_error?
+    # binding.pry
+    if self.side_one <= 0 || self.side_two <= 0 || self.side_three <= 0
+      raise TriangleError, "Testing error message"
+      #how you add a message if you want to with the error
+    end
+  end
+
+  def triangle_inequality_error?
+     if side_one + side_two <= side_three || side_two + side_three <= side_one || side_three + side_one <= side_two
+      raise TriangleError
+     end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
+
+# new_triangle = Triangle.new(2, 2, 3)
+# puts new_triangle.check_errors
